5fd8aee325f08406e547d31a373f6e0fdfef2f55
David Hahn <david@40digits.com>
2015-11-06 21:58:21 -0600

    Add actual ratings and cattribute table

--- app/models/cattribute.rb
+++ app/models/cattribute.rb
@@ -1,2 +1,3 @@
 class Cattribute < ActiveRecord::Base
+	has_many :ratings
 end
--- db/migrate/20151107034933_create_ratings.rb
+++ db/migrate/20151107034933_create_ratings.rb
@@ -1,7 +1,7 @@
 class CreateRatings < ActiveRecord::Migration
   def change
     create_table :ratings do |t|
-      t.references :user, index: true, foreign_key: true
+      t.references :cattribute, index: true, foreign_key: true
     	t.text :comment
     	t.integer :paws
       t.timestamps null: false
