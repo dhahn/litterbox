ba04b3e811ffcbb66a2f9f44f62a7c6bb59a8fe0
JC Wilcox <84jwilcox@gmail.com>
2015-11-06 21:18:24 -0600

    Add cat model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107024939) do
+ActiveRecord::Schema.define(version: 20151107031552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cats", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "description"
+    t.string   "name"
+    t.string   "breed"
+    t.string   "gender",                limit: 1
+    t.string   "spayed_slash_neutered", limit: 1
+    t.boolean  "clawed"
+    t.string   "color"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_index "cats", ["user_id"], name: "index_cats_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                            default: "", null: false
@@ -40,4 +55,5 @@ ActiveRecord::Schema.define(version: 20151107024939) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "cats", "users"
 end
