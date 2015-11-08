8fbf00b6d2ebbb7450780f5049e1b64bba07554e
JC Wilcox <84jwilcox@gmail.com>
2015-11-07 20:16:06 -0600

    fix redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
 end
