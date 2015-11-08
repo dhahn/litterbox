727dbc0b6c2bf207679c7535471a44656021a11f
JC Wilcox <84jwilcox@gmail.com>
2015-11-08 16:13:14 -0600

    litter box button

--- app/helpers/litter_boxes_helper.rb
+++ app/helpers/litter_boxes_helper.rb
@@ -1,9 +1,21 @@
 module LitterBoxesHelper
-  def litter_box_link_path(user)
-    if user && (lb = user.litter_box) && lb.persisted?
+  def litter_box_link
+    link_to litter_box_link_text, litter_box_link_path, class: 'pure-menu-link'
+  end
+
+  def litter_box_link_path
+    if current_user && (lb = current_user.litter_box) && lb.persisted?
       edit_litter_box_path(lb)
     else
       new_litter_box_path
+    end
+  end
+
+  def litter_box_link_text
+    if current_user && (lb = current_user.litter_box) && lb.persisted?
+      'Edit Litter Box'
+    else
+      'Host a Litter Box'
     end
   end
 end
