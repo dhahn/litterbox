6e49ca172123220b3d9cc6ed5f834061407fe566
Miguel Solano <miguel.solano@40digits.com>
2015-11-08 10:29:25 -0600

    Added g maps functionality on the create new litter boxes page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4487,17 +4487,10 @@ waitFor('body.litter_boxes-new', function() {
   var $litterboxNumberAdults = $("#litter_box_numbers_of_adults");
   var $litterboxNumberChildren = $("#litter_box_numbers_of_children");
   var $litterboxNumberPets = $("#litter_box_numbers_of_pets");
-  var $latitude = $("#litter_box_latitude");
-  var $longitude = $("#litter_box_longitude");
-  var myLatLng = function(){ return { lat: $latitude.val(), lng: $longitude.val() }}
-  var currentLocation = {
-    address: ' ',
-    addressTwo: '',
-    city: ' ',
-    state: ' ',
-    zip: ' ',
-    value: function() { return this.address + " " + this.addressTwo+ " " + this.city + ", " + this.state + " " +this.zip }
-  };
+  var $latitude = $("#litter_box_latitude").val();
+  var $longitude = $("#litter_box_longitude").val();
+  var myLatLng = function(){ return { lat: $latitude.val(), lng: $longitude.val() }};
+  var regularMarker = '/assets/images/regular-marker.png';
   var map;
 
   var initMap = function() {
@@ -4509,65 +4502,43 @@ waitFor('body.litter_boxes-new', function() {
       mapTypeId: google.maps.MapTypeId.ROADMAP,
     };
     map = new google.maps.Map($('#map')[0], mapOptions);
-    marker = new google.maps.Marker({
-      position: myLatLng,
-      title: "Hello World!"
-    });
+
+
     // set up custom styled map
     styledMap = new google.maps.StyledMapType(customMapStyles, {name: 'Hairbnb Map'});
     map.mapTypes.set('map_style', styledMap);
     map.setMapTypeId('map_style');
+
     var idleListener = google.maps.event.addListener(map, 'idle', function(){
-      geocode(currentLocation.value(), function(results, status){
-        map.fitBounds(results[0].geometry.viewport);
+      if(!!locationValuesCache()){
+        geocode(locationValuesCache(), function(results, status){
+          map.fitBounds(results[0].geometry.viewport);
+        });
+        $latitude = map.getCenter().lat();
+        $longitude = map.getCenter().lng();
+      }
+        google.maps.event.removeListener(idleListener);
+    });
+  };
+
+  function locationValuesCache() {
+    return $litterboxAddressOne.val() +
+      $litterboxAddressTwo.val() +
+      $litterboxCity.val() +
+      $litterboxState.val() +
+      $litterboxZip.val();
+  };
+
+  $('form input').change(function(){
+    geocode(locationValuesCache(), function(results, status) {
+      map.fitBounds(results[0].geometry.viewport);
+      icon = regularMarker;
+      marker = new google.maps.Marker({
+        position: {lat: map.getCenter().lat(), lng: map.getCenter().lng()}, 
+        map: map,
+        icon: icon
       });
-      $latitude = map.getCenter().lat();
-      $longitude = map.getCenter().lng();
     });
-  };
-
-  var setMarkers = function() {
-    console.log("Hey");
-    marker.setMap(map);
-  };
-
-  $litterboxCity.on('change', function(){
-    console.log('hey');
-    currentLocation.city = $litterboxCity.val() || ' ';
-    geocode(currentLocation.value(), function(results, status){
-      map.fitBounds(results[0].geometry.viewport);
-    });
-    setMarkers();
-  });
-
-  $litterboxAddressOne.on('change', function(){
-    console.log('hey');
-    currentLocation.address = $litterboxAddressOne.val() || ' ';
-    console.log(currentLocation)
-    geocode(currentLocation.value(), function(results, status){
-      map.fitBounds(results[0].geometry.viewport);
-    });
-    setMarkers();
-  });
-
-  $litterboxState.on('change', function(){
-    console.log('hey');
-    currentLocation.state = $litterboxState.val() || ' ' ;
-    console.log(currentLocation)
-    geocode(currentLocation.value(), function(results, status){
-      map.fitBounds(results[0].geometry.viewport);
-    });
-    setMarkers();
-  });
-
-  $litterboxZip.on('change', function(){
-    console.log('hey');
-    currentLocation.zip = $litterboxZip.val() || ' ';
-    console.log(currentLocation)
-    geocode(currentLocation.value(), function(results, status){
-      map.fitBounds(results[0].geometry.viewport);
-    });
-    setMarkers();
   });
 
   var init = function() {
