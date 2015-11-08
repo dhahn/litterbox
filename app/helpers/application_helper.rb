234395ca64a98ae923bb326803954568ee9b9c9a
David Hahn <david@40digits.com>
2015-11-08 15:30:32 -0600

    thing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,4 +33,14 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  # def avatar_url(user)
+  #   if user.avatar_url.present?
+  #     user.avatar_url
+  #   else
+  #   default_url = "#{root_url}images/guest.png"
+  #   gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+  #   "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+  #   end
+  # end
 end
